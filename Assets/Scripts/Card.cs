230e4db2fa13ba805fa240a517b1cb0b30dc4813
jrebholz7 <jrebholz7969@student.pps.net>
2026-03-30 13:54:50 -0700

    Figured out what happens + placeholder sprite

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -11,7 +11,7 @@ public class Card : MonoBehaviour
     public string card_name;
     public string description;
     public int health;
-    public int cost;
+    public int speed;
     public int damage;
     public Sprite sprite;
     public TextMeshProUGUI nameText;
@@ -28,13 +28,13 @@ public class Card : MonoBehaviour
         card_name = data.card_name;
         description = data.description;
         health = data.health;
-        cost = data.cost;
+        speed = data.speed;
         damage = data.damage;
         sprite = data.sprite;
         nameText.text = card_name;
         descriptionText.text = description;
         healthText.text = health.ToString();
-        costText.text = cost.ToString();
+        costText.text = speed.ToString();
         damageText.text = damage.ToString();
         spriteImage.sprite = sprite;
 
