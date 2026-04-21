37f69d6829534d41169421ddaccc6aca55e972bc
jrebholz7 <jrebholz7969@student.pps.net>
2026-04-21 13:49:12 -0700

    tried to get shuffle and deal working for player and AI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Mono.Cecil.Cil;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,6 +15,8 @@ public class GameManager : MonoBehaviour
     public List<Card_data> discard_pile = new List<Card_data>();
     public Canvas canvas;
     public Card blank;
+    public Card Player_card = GameObject.Find("Player_card").GetComponent<Card>();
+    public Card Ai_card = GameObject.Find("Ai_card").GetComponent<Card>();
     public Vector3 player_hand_spawnpoint;
     public Vector3 offset;
 
@@ -32,6 +36,10 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         canvas = FindAnyObjectByType<Canvas>();
+        Player_card = GameObject.Find("Player_card").GetComponent<Card>();
+        Ai_card = GameObject.Find("Ai_card").GetComponent<Card>();
+        Deal(Player_card);
+        Deal(Ai_card);
     }
 
     // Update is called once per frame
@@ -40,19 +48,30 @@ public class GameManager : MonoBehaviour
         
     }
 
-    void Deal()
+    void Deal(Card replacement)
     {
         Shuffle(player_deck);
         Shuffle(ai_deck);
         for (int i = 0; i < 5; i++)
         {
             //player_hand.Add(player_deck[i]);
-            Card current_card = Instantiate(blank, player_hand_spawnpoint + offset, Quaternion.identity, canvas.transform);
-            offset.x += 300;
-            current_card.data = player_deck[i];
-            player_deck.Remove(current_card.data);
-            player_hand.Add(current_card.data);
+            //Card current_card = Instantiate(blank, player_hand_spawnpoint + offset, Quaternion.identity, canvas.transform);
+            //offset.x += 300;
+            
+            if (replacement == Player_card)
+            {
+                replacement.data = player_deck[i];
+                Player_card.data = replacement.data;
+                player_deck.Remove(replacement.data);
+                ///player_hand.Add(current_card.data);
+            } else if (replacement == Ai_card)
+            {
+                replacement.data = ai_deck[i];
+                Ai_card.data = replacement.data;
+                ai_deck.Remove(replacement.data);
+            }
             //current_card.transform.SetParent(canvas.transform);
+            
         }
 
         for (int i = 0; i<ai_deck.Count; i++)
@@ -74,6 +93,18 @@ public class GameManager : MonoBehaviour
     }
     void AI_Turn()
     {
-        
-    }    
+        if (Ai_card.data.health <= 0)
+        {
+            Deal(Ai_card);
+        }
+        Player_card.data.health -= Ai_card.data.damage;
+    }
+    void Player_Turn()
+    {
+        if (Player_card.data.health <= 0)
+        {
+            Deal(Player_card);
+        }
+        Ai_card.data.health -= Player_card.data.damage;
+    }
 }
